57365628001d9add4f51508e2ac528241f5e26ef
jabss <jabrijuhinin93@gmail.com>
2025-04-21 17:14:43 +0800

    refactored displaying anecdotes list to its own component(AnecdoteList.jsx), exercise 6.8 completed)

---
+++ src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { increaseVoteOf } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state.anecdotes)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+      dispatch(increaseVoteOf(id))
+  }
+
+  return (
+    <div>
+        <h2>Anecdotes</h2>
+        {[...anecdotes]
+        .sort((a,b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
