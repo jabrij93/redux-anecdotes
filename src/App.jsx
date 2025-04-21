f9fd8e364f5de33ecceae0531166d20b91d0a89a
jabss <jabrijuhinin93@gmail.com>
2025-04-21 17:03:57 +0800

    move adding new anecdotes into AnecdoteForm.jsx component(exercise 6.7 completed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote } from './reducers/anecdoteReducer'
 import { increaseVoteOf } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state.anecdotes)
@@ -8,14 +8,6 @@ const App = () => {
 
   const vote = (id) => {
     dispatch(increaseVoteOf(id))
-  }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-
-    dispatch(createAnecdote(content))
   }
 
   return (
@@ -34,11 +26,7 @@ const App = () => {
             </div>
           </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+     <AnecdoteForm />
     </div>
   )
 }
