4961ab01369e2b3d0e1cebd502b3295ff3fdf70b
jabss <jabrijuhinin93@gmail.com>
2025-04-22 21:20:36 +0800

    filter functionality now working(exercise 6.9 completed)

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increaseVoteOf } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.anecdotes)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    return anecdotes.filter(anecdote =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  })
+      
   const dispatch = useDispatch()
 
   const vote = (id) => {
