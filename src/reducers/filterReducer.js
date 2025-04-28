1c3494ad6803b9a4d2667a91bf5a10d10db89044
jabss <jabrijuhinin93@gmail.com>
2025-04-28 15:53:26 +0800

    refactor filterReducer to using filterSlice(working) - this and previous commit means exercise 6.10 completed

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,19 +1,67 @@
-const filterReducer = (state = '', action) => {
-    console.log('ACTION: ', action)
+import { createSlice, current } from '@reduxjs/toolkit'
 
-    switch (action.type) {
-        case 'SET_FILTER':
-          return action.payload
-        default:
-          return state
-      }
-  }
+// const filterReducer = (state = '', action) => {
+//     console.log('ACTION: ', action)
+
+//     switch (action.type) {
+//         case 'SET_FILTER':
+//           return action.payload
+//         default:
+//           return state
+//       }
+//   }
   
-  export const filterChange = filter => {
-    return {
-      type: 'SET_FILTER',
-      payload: filter,
+//   export const filterChange = filter => {
+//     return {
+//       type: 'SET_FILTER',
+//       payload: filter,
+//     }
+//   }
+
+  const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+      filterChange(state, action) {
+        return action.payload
+      },
     }
-  }
+  })
+
+  export const { filterChange } = filterSlice.actions
+  export default filterSlice.reducer
   
-  export default filterReducer
+
+  // const anecdoteSlice = createSlice({
+  //   name: 'anecdotes',
+  //   initialState,
+  //   reducers: {
+  //     createAnecdote(state, action) {
+  //       const anecdote = action.payload
+  //       state.push({
+  //         content : anecdote,
+  //         votes: 0,
+  //         id: getId()
+  //       })
+  //     },
+  //     increaseVoteOf(state, action) {
+  //       const id = action.payload
+  //       const anecdoteToVote = state.find(n => n.id === id)
+  //       const changedAnecdote = { 
+  //         ...anecdoteToVote, 
+  //         votes: anecdoteToVote.votes + 1
+  //       }
+  
+  //       console.log("NOW CURRENT",current(state))
+  
+  //       return state.map(anecdote =>
+  //         anecdote.id !== id ? anecdote : changedAnecdote
+  //       )     
+  //     }
+  //   },
+  // })
+
+  // export const { createAnecdote, increaseVoteOf } = anecdoteSlice.actions
+  // export default anecdoteSlice.reducer
+  
+  // export default filterReducer
