840fff0076d8a2d7fcc614184bfe780ba79f065b
jabss <jabrijuhinin93@gmail.com>
2025-04-29 14:07:55 +0800

    refactor notificationReducer to use createSlice (exercise 6.13 completed)

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,26 +1,43 @@
-const notificationReducer = (state = '', action) => {
-    switch (action.type) {
-      case 'SET_NOTIFICATION':
-        return action.payload
-      case 'CLEAR_NOTIFICATION':
-        return ''
-      default:
-        return state
+import { createSlice, current } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+    clearNotification() {
+      return ''
     }
   }
+})
+
+export const { setNotification, clearNotification } = notificationSlice.actions
+export default notificationSlice.reducer
+
+// const notificationReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case 'SET_NOTIFICATION':
+//       return action.payload
+//     case 'CLEAR_NOTIFICATION':
+//       return ''
+//     default:
+//       return state
+//   }
+// }
   
-  export const setNotification = (content) => {
-    return {
-      type: 'SET_NOTIFICATION',
-      payload: content,
-    }
-  }
+// export const setNotification = (content) => {
+//   return {
+//     type: 'SET_NOTIFICATION',
+//     payload: content,
+//   }
+// }
   
-  export const clearNotification = () => {
-    return {
-      type: 'CLEAR_NOTIFICATION',
-    }
-  }
+// export const clearNotification = () => {
+//   return {
+//     type: 'CLEAR_NOTIFICATION',
+//   }
+// }
   
-  export default notificationReducer
-  
+// export default notificationReducer 
