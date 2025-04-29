adbeb00be84b052a817aa07aa6a3512a335e209e
jabss <jabrijuhinin93@gmail.com>
2025-04-29 17:56:30 +0800

    adding new anecdotes working ! (exercise 6.15 completed)

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,16 +1,21 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 import { setNotification, clearNotification } from '../reducers/notificationReducer'
+
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
 
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote.content))
+    console.log('NEW-ANECDOTE', newAnecdote) // Check that `newAnecdote.content` is a string
+
     dispatch(setNotification(content))
 
     setTimeout(() => {
