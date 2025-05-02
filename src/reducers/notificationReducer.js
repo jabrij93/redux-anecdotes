3f06e3d77f36b298df02e810380acde3916cbc4d
jabss <jabrijuhinin93@gmail.com>
2025-05-02 17:32:03 +0800

    remove old anecdote and notification reducer

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -32,29 +32,3 @@ export const setNotification = (message, seconds) => {
 }
 
 export default notificationSlice.reducer
-
-// const notificationReducer = (state = '', action) => {
-//   switch (action.type) {
-//     case 'SET_NOTIFICATION':
-//       return action.payload
-//     case 'CLEAR_NOTIFICATION':
-//       return ''
-//     default:
-//       return state
-//   }
-// }
-  
-// export const setNotification = (content) => {
-//   return {
-//     type: 'SET_NOTIFICATION',
-//     payload: content,
-//   }
-// }
-  
-// export const clearNotification = () => {
-//   return {
-//     type: 'CLEAR_NOTIFICATION',
-//   }
-// }
-  
-// export default notificationReducer 
